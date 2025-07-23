a16d00a6b66f50ac36f3f61306202c9335f1bc2c
Venkat <subba.rao@slickbit.com>
2025-07-23 13:05:53 +0530

    Update toast.tsx

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,7 +14,8 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      // Center horizontally at bottom, full width on mobile, max width on desktop
+      "fixed bottom-0 left-1/2 transform -translate-x-1/2 z-[100] flex flex-col items-center w-full max-w-full p-4 md:max-w-[420px]",
       className
     )}
     {...props}
